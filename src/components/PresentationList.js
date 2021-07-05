02d171eb8128c09c7d0a0ed4d3b6b648ef58538c
ata <ata@geospline.ru>
2021-07-05 17:23:23 +0300

    (IW1D) Support presentations list from server

--- src/components/PresentationList.js
+++ src/components/PresentationList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import RecursiveTreeView from './RecursiveTreeView';
 
 export class PresentationList extends Component {
-    static displayName = PresentationList.name;
 
     constructor(props) {
         super(props);
@@ -16,22 +16,9 @@ export class PresentationList extends Component {
 
     static renderPresentationList(presentationsJSON) {
         return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>groupDisplayName</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {presentationsJSON.dockPresentationsManager.client.map(presentation =>
-                        <tr>
-                            <td>{presentation.name}</td>
-                            <td>{presentation.groupDisplayName}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+                <div>
+                { RecursiveTreeView(presentationsJSON)}
+                </div>
         );
     }
 
@@ -48,7 +35,6 @@ export class PresentationList extends Component {
         return (
             <div>
                 <h1 id="tabelLabel" >React WMW</h1>
-                <p>Presentations</p>
                 {contents}
             </div>
         );
---
+++ src/components/RecursiveTreeView.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+
+export default function RecursiveTreeView(data) {
+
+    const renderTree = (nodes) => (
+        <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.displayName}>
+            {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+        </TreeItem>
+    );
+
+    return (
+        <TreeView
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpanded={['root']}
+            defaultExpandIcon={<ChevronRightIcon />}
+        >
+            {Array.isArray(data.children) ? data.children.map((node) => renderTree(node)) : null}
+        </TreeView>
+    );
+}
