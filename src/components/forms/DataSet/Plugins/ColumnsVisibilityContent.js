08265a266eac8c398ecba798dc05f4c815bd0581
ata <ata@geospline.ru>
2022-01-03 16:10:42 +0300

    (ID2x) Support groups for column visibility

--- src/components/forms/DataSet/Plugins/ColumnsVisibilityContent.js
+++ src/components/forms/DataSet/Plugins/ColumnsVisibilityContent.js
@@ -6,34 +6,76 @@ import setFormSettings from "../../../../store/actionCreators/setFormSettings";
 export default function ColumnsVisibilityContent(props) {
     const dispatch = useDispatch();
     const { formId } = props;
-    const formRef = useSelector((state) => state.formRefs[formId]);
+    const formProperties = useSelector((state) => state.formRefs[formId].current?.properties());
     const tableSettings = useSelector((state) => state.formSettings[formId]);
+    const [columnGroupingData, setColumnGroupingData] = React.useState([]);
+    const [columnListData, setColumnListData] = React.useState([]);
 
-    const [tree, setTree] = React.useState(formRef?.current?.properties()?.map(property => {
+    const getColumnForTree = col => {
         return {
-            id: property.name,
-            text: property.displayName,
+            id: col.name,
+            text: col.displayName,
             checked: (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") ?
-                !tableSettings?.attachedProperties?.exclude.includes(property.name) :
-                tableSettings?.attachedProperties?.exclude.includes(property.name)
+                !tableSettings?.attachedProperties?.exclude.includes(col.name) :
+                tableSettings?.attachedProperties?.exclude.includes(col.name)
         }
-    }));
+    };
+
+    React.useEffect(() => {
+        var listData = [];
+        var groupingData = [];
+        formProperties?.forEach(col => {
+            if (!col.treePath || col.treePath.length === 0) {
+                const columnForTree = getColumnForTree(col);
+                groupingData.push(columnForTree);
+                listData.push(columnForTree);
+            }
+            else {
+                var parent = null;
+                col.treePath.forEach(part => {
+                    var trimPart = part.trim();
+                    var parentArray = parent?.items ?? groupingData;
+                    parent = parentArray.find(p => p?.id === trimPart);
+                    if (!parent) {
+                        var children = [];
+                        parent = {
+                            id: trimPart,
+                            text: trimPart,
+                            expanded: true,
+                            items: children
+                        };
+                        parentArray.push(parent);
+                    }
+                });
+                const columnForTree = getColumnForTree(col);
+                parent.items.push(columnForTree);
+                listData.push(columnForTree);
+            }
+        });
+        setColumnListData(listData);
+        setColumnGroupingData(groupingData);
+    }, [formProperties]);
 
     const onCheckChange = (event) => {
-        event.item.checked = !event.item.checked;
-        setTree([...tree]);
-        if (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") {
-            tableSettings.attachedProperties.exclude = tree.filter(ti => !ti.checked).map(ti => ti.id);
+        if (event.item.items) {
+
         }
         else {
-            tableSettings.attachedProperties.exclude = tree.filter(ti => ti.checked).map(ti => ti.id);
+            event.item.checked = !event.item.checked;
+            setColumnGroupingData([...columnGroupingData]);
+            if (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") {
+                tableSettings.attachedProperties.exclude = columnListData.filter(ti => !ti.checked).map(ti => ti.id);
+            }
+            else {
+                tableSettings.attachedProperties.exclude = columnListData.filter(ti => ti.checked).map(ti => ti.id);
+            }
+            dispatch(setFormSettings(formId, { ...tableSettings }));
         }
-        dispatch(setFormSettings(formId, { ...tableSettings }));
     };
 
     return (
         <TreeView className="popuptreeview"
-            data={tree}
+            data={columnGroupingData}
             checkboxes={true}
             onCheckChange={onCheckChange}
         />);
