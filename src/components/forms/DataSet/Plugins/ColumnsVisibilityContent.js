405d6214223a9ee96cc255dcd35f401bd5115ad1
ata <ata@geospline.ru>
2022-01-04 11:54:05 +0300

    (ID2x) Fixed column groups visibility

--- src/components/forms/DataSet/Plugins/ColumnsVisibilityContent.js
+++ src/components/forms/DataSet/Plugins/ColumnsVisibilityContent.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { TreeView } from "@progress/kendo-react-treeview";
+import {
+    TreeView,
+    processTreeViewItems,
+    handleTreeViewCheckChange,
+} from "@progress/kendo-react-treeview";
 import setFormSettings from "../../../../store/actionCreators/setFormSettings";
 
 export default function ColumnsVisibilityContent(props) {
@@ -10,72 +14,107 @@ export default function ColumnsVisibilityContent(props) {
     const tableSettings = useSelector((state) => state.formSettings[formId]);
     const [columnGroupingData, setColumnGroupingData] = React.useState([]);
     const [columnListData, setColumnListData] = React.useState([]);
+    const [check, setCheck] = React.useState({
+        ids: [],
+        applyCheckIndeterminate: true,
+    });
 
-    const getColumnForTree = col => {
+    const setChecking = (col, index, indices) => {
+        if (col.items) {
+            col.items.forEach((item, innerIndex) => {
+                setChecking(item, index + '_' + innerIndex, indices);
+            });
+        }
+        else {
+            const checked = (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") ?
+                !tableSettings?.attachedProperties?.exclude.includes(col.id) :
+                tableSettings?.attachedProperties?.exclude.includes(col.id);
+            if (checked) {
+                indices.push(index);
+            }
+        }
+    };
+
+    const getColumnForTree = (col, index) => {
         return {
+            index: index,
             id: col.name,
-            text: col.displayName,
-            checked: (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") ?
-                !tableSettings?.attachedProperties?.exclude.includes(col.name) :
-                tableSettings?.attachedProperties?.exclude.includes(col.name)
+            text: col.displayName
         }
     };
 
     React.useEffect(() => {
         var listData = [];
         var groupingData = [];
-        formProperties?.forEach(col => {
-            if (!col.treePath || col.treePath.length === 0) {
-                const columnForTree = getColumnForTree(col);
-                groupingData.push(columnForTree);
-                listData.push(columnForTree);
-            }
-            else {
-                var parent = null;
-                col.treePath.forEach(part => {
-                    var trimPart = part.trim();
-                    var parentArray = parent?.items ?? groupingData;
-                    parent = parentArray.find(p => p?.id === trimPart);
-                    if (!parent) {
-                        var children = [];
-                        parent = {
-                            id: trimPart,
-                            text: trimPart,
-                            expanded: true,
-                            items: children
-                        };
-                        parentArray.push(parent);
-                    }
-                });
-                const columnForTree = getColumnForTree(col);
-                parent.items.push(columnForTree);
-                listData.push(columnForTree);
-            }
-        });
+        if (formProperties) {
+            formProperties.forEach(col => {
+                if (!col.treePath || col.treePath.length === 0) {
+                    let index = groupingData.length + '';
+                    const columnForTree = getColumnForTree(col, index);
+                    groupingData.push(columnForTree);
+                    listData.push(columnForTree);
+                }
+                else {
+                    var parent = null;
+                    col.treePath.forEach(part => {
+                        var trimPart = part.trim();
+                        var parentArray = parent?.items ?? groupingData;
+                        let index = parent?.items ? parent.index + '_' + parentArray.length : parentArray.length + '';
+                        parent = parentArray.find(p => p?.id === trimPart);
+                        if (!parent) {
+                            var children = [];
+                            parent = {
+                                index: index,
+                                id: trimPart,
+                                text: trimPart,
+                                expanded: true,
+                                items: children
+                            };
+                            parentArray.push(parent);
+                        }
+                    });
+                    let index = parent.index + '_' + parent.items.length;
+                    const columnForTree = getColumnForTree(col, index);
+                    parent.items.push(columnForTree);
+                    listData.push(columnForTree);
+                }
+            });
+        }
         setColumnListData(listData);
         setColumnGroupingData(groupingData);
-    }, [formProperties]);
+        var indices = [];
+        groupingData.forEach((item, innerIndex) => {
+            setChecking(item, innerIndex + '', indices);
+        });
+        setCheck({
+            ids: indices,
+            applyCheckIndeterminate: true,
+        });
+    }, [formProperties, setChecking]);
 
     const onCheckChange = (event) => {
-        if (event.item.items) {
+        const settings = {
+            singleMode: false,
+            checkChildren: true,
+            checkParents: true,
+        };
+        const newCheck = handleTreeViewCheckChange(event, check, columnGroupingData, settings);
+        setCheck(newCheck);
 
+        if (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") {
+            tableSettings.attachedProperties.exclude = columnListData.filter(ti => !newCheck.ids.includes(ti.index)).map(ti => ti.id);
         }
         else {
-            event.item.checked = !event.item.checked;
-            setColumnGroupingData([...columnGroupingData]);
-            if (tableSettings?.attachedProperties?.attachOption !== "AttachNothing") {
-                tableSettings.attachedProperties.exclude = columnListData.filter(ti => !ti.checked).map(ti => ti.id);
-            }
-            else {
-                tableSettings.attachedProperties.exclude = columnListData.filter(ti => ti.checked).map(ti => ti.id);
-            }
-            dispatch(setFormSettings(formId, { ...tableSettings }));
+            tableSettings.attachedProperties.exclude = columnListData.filter(ti => newCheck.ids.includes(ti.index)).map(ti => ti.id);
         }
+        dispatch(setFormSettings(formId, { ...tableSettings }));
     };
 
     return (
         <TreeView className="popuptreeview"
-            data={columnGroupingData}
+            data={processTreeViewItems(columnGroupingData, {
+                check: check,
+            })}
             checkboxes={true}
             onCheckChange={onCheckChange}
         />);
