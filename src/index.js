a93bf25c5dac9ec6897319fbddb49338e5b995f8
ata <ata@geospline.ru>
2022-04-25 10:53:55 +0300

    (ID2x) Define Object.fromEntries for old browser versions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ import './i18n';
 function component() {
   const element = document.createElement('div');
 
+  if (!Object.fromEntries) {
+    Object.fromEntries = (entries) => {
+      var result = {};
+      entries.forEach((entry) => {
+        var [key, value] = entry;
+        result[key] = value;
+      });
+      return result;
+    }
+  }
+
   ReactDOM.render(
     <App />,
   element);
