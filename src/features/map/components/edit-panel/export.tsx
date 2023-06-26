bf8dd7e8f48db4835629d09a6eeb2ce7280a38ed
vin <vitalok88@gmail.com>
2023-06-26 12:13:26 +0300

    Remove "canvas2svg" library

--- src/features/map/components/edit-panel/export.tsx
+++ src/features/map/components/edit-panel/export.tsx
@@ -1,7 +1,7 @@
 import { TFunction } from 'react-i18next';
-import { MenuSection, BigButton } from '../../../../shared/ui';
-import { jsPDF } from 'jspdf'
-import pdfIcon from '../../../..//assets/images/map/pdf.png';
+import { MenuSection, BigButton } from 'shared/ui';
+import { jsPDF } from 'jspdf';
+import pdfIcon from 'assets/images/map/pdf.png';
 
 
 interface ActionsProps {
@@ -14,10 +14,11 @@ export const Export = ({mapState, t}: ActionsProps) => {
   const { canvas, mapData } = mapState;
 
   const exportToPDF = () => {
-    const doc = new jsPDF({unit: 'px', orientation: 'l', format: [canvas.width, canvas.height + 25]});
+    const format = [canvas.width, canvas.height];
+    const doc = new jsPDF({unit: 'px', orientation: 'l', format});
     const mapName = mapData?.mapName || 'Map', date = mapData?.date || '';
 
-    const imgData = canvas.toDataURL("image/jpeg", 1.0);
+    const imgData = canvas.toDataURL('image/jpeg', 1.0);
     doc.addImage(imgData, 'JPEG', 0, 0, canvas.width, canvas.height);
     doc.text(date, 5, 15);
     doc.save(mapName + '.pdf')
@@ -25,7 +26,10 @@ export const Export = ({mapState, t}: ActionsProps) => {
 
   return (
     <MenuSection header={t('map.actions.header')} className={'map-actions'}>
-      <BigButton text={t('map.actions.export')} icon={pdfIcon} action={exportToPDF} disabled={!mapState?.isLoadSuccessfully}/>
+      <BigButton
+        text={t('map.actions.export')} icon={pdfIcon}
+        action={exportToPDF} disabled={!mapState?.isLoadSuccessfully}
+      />
     </MenuSection>
   );
 };
