43a085341d239e0168de5e46df0dece03e2079a6
vin <vitalok88@gmail.com>
2024-02-19 20:21:57 +0300

    Fix build config for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
-import { ConfigEnv, UserConfig, defineConfig } from 'vite';
+import { ConfigEnv, UserConfig, AliasOptions, PluginOption, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wellManagerSystemPlugin from './build-utils/wm-system-plugin.js';
 
 
+const aliasOptions: AliasOptions = [
+  {find: 'app/', replacement: '/src/app/'},
+  {find: 'assets/', replacement: '/src/assets/'},
+  {find: 'widgets/', replacement: '/src/widgets/'},
+  {find: 'features/', replacement: '/src/features/'},
+  {find: 'entities/', replacement: '/src/entities/'},
+  {find: 'shared/', replacement: '/src/shared/'},
+];
+
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}: ConfigEnv): UserConfig => ({
-  base: mode === 'prod' ? '/PATH_TO_REPLACE/' : './',
-  plugins: [
-    react(),
-    mode === 'prod' ? undefined : wellManagerSystemPlugin(command),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: './build',
-    sourcemap: true,
-    assetsInlineLimit: 512,
-  },
-  esbuild: {
-    // IIS по умолчанию не поддерживает UTF-8
-    charset: 'ascii',
-  },
-  resolve: {
-    alias: [
-      {find: 'app/', replacement: '/src/app/'},
-      {find: 'assets/', replacement: '/src/assets/'},
-      {find: 'widgets/', replacement: '/src/widgets/'},
-      {find: 'features/', replacement: '/src/features/'},
-      {find: 'entities/', replacement: '/src/entities/'},
-      {find: 'shared/', replacement: '/src/shared/'},
-    ],
-  },
-  assetsInclude: ['**/*.bin', '**/*.def'],
-}));
+export default defineConfig((env: ConfigEnv): UserConfig => {
+  const devMode = env.mode !== 'prod';
+  const plugins: PluginOption[] = [react()];
+  if (devMode) plugins.push(wellManagerSystemPlugin(env.command));
+
+  return {
+    base: devMode ? './' : '/PATH_TO_REPLACE/',
+    plugins: plugins,
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: './build',
+      sourcemap: devMode,
+      assetsInlineLimit: 512,
+    },
+    esbuild: {
+      // IIS по умолчанию не поддерживает UTF-8
+      charset: 'ascii',
+    },
+    resolve: {
+      alias: aliasOptions,
+    },
+    assetsInclude: ['**/*.bin', '**/*.def'],
+  };
+});
